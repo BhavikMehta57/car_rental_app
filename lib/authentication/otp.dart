1a13f25cd9a86c3da7a6f2f93c8f96b063c86426
BhavikMehta57 <shadow7bm@gmail.com>
2022-04-24 06:04:27 +0530

    web

--- lib/authentication/otp.dart
+++ lib/authentication/otp.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:car_rental_app/widgets/widgets.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
@@ -37,7 +38,7 @@ class _OtpState extends State<Otp> {
     }
     _timer = Timer.periodic(
       Duration(seconds: 1),
-      (_timer) {
+          (_timer) {
         setState(() {
           if (_counter > 0) {
             _counter--;
@@ -67,30 +68,40 @@ class _OtpState extends State<Otp> {
   sendOTP() async {
     print("Indside Send OPT");
     try {
-      _auth.verifyPhoneNumber(
-        phoneNumber: phone,
-        timeout: Duration(seconds: 60),
-        verificationCompleted: (AuthCredential credential) async {
-          print("verification completed call back called from OPT SCREEEN !!!");
-          widget.onVerificationSuccess(credential);
-        },
-        verificationFailed: (FirebaseAuthException exception) {
-          print(exception.code);
-          print("Verification Failed !! from OTP SCREEN ");
-          print(exception);
-          widget.onVerificationFailure();
-        },
-        codeSent: (String verificationId, [int forceResendToken]) {
-          this.verificationId = verificationId;
-          print(verificationId);
-          print("Code Sent !!");
-          setState(() {codeSent = true;});
-        },
-        codeAutoRetrievalTimeout:(val) {
-          verificationId = val;
-          print("codeAutoRetrievalTimeout\n" + val);
-        },
-      );
+      if(kIsWeb){
+        _auth.signInWithPhoneNumber(phone);
+        ConfirmationResult confirmationResult = await _auth.signInWithPhoneNumber(phone, RecaptchaVerifier(
+          container: 'reCaptcha',
+          size: RecaptchaVerifierSize.compact,
+          theme: RecaptchaVerifierTheme.dark,
+        ));
+        UserCredential userCredential = await confirmationResult.confirm('123456');
+      }
+      else{
+        _auth.verifyPhoneNumber(
+          phoneNumber: phone,
+          timeout: Duration(seconds: 60),
+          verificationCompleted: (AuthCredential credential) async {
+            print("verification completed call back called from OPT SCREEEN !!!");
+            widget.onVerificationSuccess(credential);
+          },
+          verificationFailed: (FirebaseAuthException exception) {
+            print(exception.code);
+            print("Verification Failed !! from OTP SCREEN ");
+            print(exception);
+            widget.onVerificationFailure();
+          },
+          codeSent: (String verificationId, [int forceResendToken]) {
+            this.verificationId = verificationId;
+            print(verificationId);
+            print("Code Sent !!");
+            setState(() {codeSent = true;});
+          },
+          codeAutoRetrievalTimeout:(val) {
+            verificationId = val;
+            print("codeAutoRetrievalTimeout\n" + val);
+          },
+        );}
     } catch (e) {
       print(e);
       final snackBar = SnackBar(
@@ -146,66 +157,66 @@ class _OtpState extends State<Otp> {
                   16.height,
                   _counter != 0
                       ? Text.rich(
-                          TextSpan(
-                            text: 'Re-send after',
-                            style: secondaryTextStyle(),
-                            children: <TextSpan>[
-                              TextSpan(
-                                  text: ' $_counter seconds',
-                                  style: boldTextStyle(
-                                      color: Colors.black, size: 14)),
-                            ],
-                          ),
-                        ).center()
+                    TextSpan(
+                      text: 'Re-send after',
+                      style: secondaryTextStyle(),
+                      children: <TextSpan>[
+                        TextSpan(
+                            text: ' $_counter seconds',
+                            style: boldTextStyle(
+                                color: Colors.black, size: 14)),
+                      ],
+                    ),
+                  ).center()
                       : Text("Resend OTP").center().onTap(() {
-                          //resent OTP
-                          _startTimer();
-                          verificationId="";
-                          setState(() {codeSent = false;});
-                          sendOTP();
-                        }),
+                    //resent OTP
+                    _startTimer();
+                    verificationId="";
+                    setState(() {codeSent = false;});
+                    sendOTP();
+                  }),
                   16.height,
                   isLoading
-                  ?
+                      ?
                   CircularProgressIndicator()
-                  :
+                      :
                   Padding(
                     padding: const EdgeInsets.fromLTRB(40, 0, 40, 0),
                     child: GestureDetector(
-                          child: CustomButton(
-                          text: "Verify",),
-                          onTap: () async {
-                            final code = _codeController.text.trim();
-                            if(code.length != 6){
-                              final snackBar = SnackBar(
-                                content: Text('Enter Valid OTP !'),
-                                duration: Duration(seconds: 3),
-                              );
-                              ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                              return;
-                            }
-                            else{
-                              /// Verify OTP Manually !
-                              setState(() {
-                                isLoading=true;
-                              });
-                              String result = await widget.verifyButtonOnTap(verificationId, code);
-                              setState(() {
-                                isLoading=false;
-                              });
+                      child: CustomButton(
+                        text: "Verify",),
+                      onTap: () async {
+                        final code = _codeController.text.trim();
+                        if(code.length != 6){
+                          final snackBar = SnackBar(
+                            content: Text('Enter Valid OTP !'),
+                            duration: Duration(seconds: 3),
+                          );
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                          return;
+                        }
+                        else{
+                          /// Verify OTP Manually !
+                          setState(() {
+                            isLoading=true;
+                          });
+                          String result = await widget.verifyButtonOnTap(verificationId, code);
+                          setState(() {
+                            isLoading=false;
+                          });
 
-                              if(result != "Success"){
-                                final snackBar = SnackBar(
-                                content: Text(result),
-                                duration: Duration(seconds: 3),
-                                );
-                                ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                                return;
-                              }
-                            }
-                          },
-                        ),
+                          if(result != "Success"){
+                            final snackBar = SnackBar(
+                              content: Text(result),
+                              duration: Duration(seconds: 3),
+                            );
+                            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                            return;
+                          }
+                        }
+                      },
                     ),
+                  ),
                 ],
               ).paddingAll(16),
             ),
