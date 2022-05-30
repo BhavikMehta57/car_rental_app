f1c7b8671d743dd940fd2c671b10b759448eeda9
BhavikMehta57 <shadow7bm@gmail.com>
2022-05-30 18:16:45 +0530

    Final

--- lib/configMaps.dart
+++ lib/configMaps.dart
@@ -1,3 +1,3 @@
 String apiKey = 'YOUR_ANDROID/iOS_APK_API_KEY';
-String geocodingApi = 'AIzaSyDN10Hbi3k1DzWlnb7rSYwimgL_Nsn2gCQ';
+String geocodingApi = 'YOURAPIKEY';
 //(This geocoding api also works for places api and directions api if you have restricted the api key with these 3 services)
--- lib/main.dart
+++ lib/main.dart
@@ -13,26 +13,7 @@ import 'package:car_rental_app/services/authentication_service.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  // final FirebaseApp app = await Firebase.initializeApp(
-  //   name: 'carrentalapp',
-  //   options: Platform.isIOS || Platform.isWindows
-  //       ? FirebaseOptions(
-  //     appId: '1:297855924061:ios:c6de2b69b03a5be8',
-  //     apiKey: 'AIzaSyD_shO5mfO9lhy2TVWhfo1VUmARKlG4suk',
-  //     projectId: 'flutter-firebase-plugins',
-  //     messagingSenderId: '297855924061',
-  //     databaseURL: 'https://flutterfire-cd2f7.firebaseio.com',
-  //   )
-  //       : FirebaseOptions(
-  //       apiKey: "AIzaSyBn2hyZV0aOUOgLj2Cr4yTzrNwhCOXdDXk",
-  //       authDomain: "carrentalapp-6bc1f.firebaseapp.com",
-  //       projectId: "carrentalapp-6bc1f",
-  //       storageBucket: "carrentalapp-6bc1f.appspot.com",
-  //       messagingSenderId: "106281468330",
-  //       appId: "1:106281468330:web:8fea2c2b60479d6448ebd3",
-  //       measurementId: "G-MKVYNRVMF7"
-  //   ),
-  // );
+
   runApp(MyApp());
 }
 
